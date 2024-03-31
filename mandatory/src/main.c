8c892c2326d7c30cb89912433185b71fb5891bef
Ofacy <lcottet@student.42lyon.fr>
2024-03-31 20:23:56 +0200

    fix

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -6,26 +6,9 @@
 /*   By: lcottet <lcottet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 22:35:32 by tdameros          #+#    #+#             */
-/*   Updated: 2024/03/31 20:05:46 by lcottet          ###   ########.fr       */
+/*   Updated: 2024/03/31 20:23:49 by lcottet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-#include "game.h"
-#include "error.h"
-
-static int	get_fd(int argc, char **argv);
-
-int	main(int argc, char **argv)
-{
-	int		fd;
-	t_game	game;
-
-	fd = get_fd(argc, argv);
-	if (fd == -1)
 	{
 		print_error(GENERIC_ERROR);
 		return (1);
